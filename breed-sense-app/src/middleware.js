e412fb2f9f5ea89419d79b7a5b30289292891e8d
rajritik4041 <rajritik.9236@gmail.com>
2025-11-15 16:07:58 +0530

    update a mail part 3

--- breed-sense-app/src/middleware.js
+++ breed-sense-app/src/middleware.js
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" ||  path === "/" || path === "/signup";
+  const isPublicPath = path === "/login" || path === "/" || path === "/signup";
 
   const token = request.cookies.get("token")?.value || '';
 
   // Already logged in → cannot go to login/signup
   if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/profile", request.url));
+    return NextResponse.redirect(new URL("/profile", request.url)), NextResponse.redirect(new URL("/profit", request.url)), NextResponse.redirect(new URL("/details", request.url)), NextResponse.redirect(new URL("/contactus", request.url));
   }
 
   // Not logged in → cannot access private pages
